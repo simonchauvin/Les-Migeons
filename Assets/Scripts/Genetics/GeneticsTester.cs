b113e29604ef54cf13ce7b82668818fb9973de0b
guillaume <guigui_3d@yahoo.fr>
2015-01-24 13:16:09 +0100

    Genetics added

---
+++ Assets/Scripts/Genetics/GeneticsTester.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneticsTester : MonoBehaviour {
+
+    public Genetics.MIGEON_ACTION[] actions;
+
+	// Use this for initialization
+	void Start () {
+        actions = Genetics.createActions(10);
+        for(int i=0;i<actions.Length;i++)
+            Debug.Log(actions[i]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
