d1b2ffaa0f90cf2c3f65f5e6964a87476d634868
guillaume <guigui_3d@yahoo.fr>
2015-01-24 14:46:09 +0100

    Genetics qui tue

--- Assets/Scripts/Genetics/GeneticsTester.cs
+++ Assets/Scripts/Genetics/GeneticsTester.cs
@@ -3,17 +3,36 @@ using System.Collections;
 
 public class GeneticsTester : MonoBehaviour {
 
-    public Genetics.GeneticCode code;
+    public Genetics.GeneticCode code1;
+    public Genetics.GeneticCode code2;
 
 	// Use this for initialization
 	void Start () {
-        code = Genetics.makeGeneticCode();
-        for(int i=0;i<code.actions.Length;i++)
-            Debug.Log(code.actions[i]);
+        code1 = Genetics.makeGeneticCode();
+        code1.showCodeDebug();
+        Genetics.mutate(ref code1);
+        code1.showCodeDebug();
+        code2 = Genetics.makeGeneticCode();
+        code2.showCodeDebug();
+        Genetics.mutate(ref code2);
+        code2.showCodeDebug();
+
+
+        Genetics.GeneticCode code3 = Genetics.crossOver(code1, code2);
+        code1.showCodeDebug();
+        code2.showCodeDebug();
+        code3.showCodeDebug();
+
+        code3 = Genetics.crossOver(code1, code2);
+        code3.showCodeDebug();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
 	    
 	}
 }
